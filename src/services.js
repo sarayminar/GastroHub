4edc0183b8f5d80d94d9879796e900d1b9393af6
sarayminar <sarayminar@gmail.com>
2025-02-25 09:55:13 +0100

    edit method

--- src/services.js
+++ src/services.js
@@ -3,6 +3,14 @@ const urlBase = "http://localhost:3000/platos/";
 //Método POST
 function loadCreateForm() {
   resetBody();
+  loadForm();
+  document.getElementById("recipe-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    createRecipe();
+  });
+}
+
+function loadForm() {
   const createForm = document.createElement("form");
   createForm.setAttribute("id", "recipe-form");
   createForm.innerHTML = `
@@ -16,13 +24,8 @@ function loadCreateForm() {
     <input type="text" id="recipe-ingredients" name="recipe-ingredients"> *(Separe con ',' cada ingrediente)<br><br>
     <label for="recipe-procedure">Procedimiento:</label>
     <textarea id="recipe-procedure" name="recipe-procedure"></textarea><br><br>
-    <input type="submit" value="Crear">
-  `;
+    <input type="submit" value="Enviar">`;
   document.body.appendChild(createForm);
-  createForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    createRecipe();
-  });
 }
 
 async function createRecipe() {
@@ -36,7 +39,8 @@ async function createRecipe() {
     return;
   }
 
-  const response = await postRecipe(recipeName, recipeOrigin, recipeImage, recipeIngredients, recipeProcedure);
+  const response = await postRecipe( recipeName, recipeOrigin, recipeImage, recipeIngredients, recipeProcedure);
+
   if (!response.ok) {
     alert("Error al guardar en base de datos");
   }
@@ -47,15 +51,14 @@ async function createRecipe() {
   document.body.appendChild(p);
 }
 
-function validateFieldsForm(recipeName, recipeOrigin, recipeImage, recipeIngredients, recipeProcedure) {
+function validateFieldsForm(recipeName,recipeOrigin,recipeImage,recipeIngredients,recipeProcedure) {
   if (recipeName.length == 0 || recipeOrigin.length == 0 || recipeImage.length == 0 || recipeIngredients.length == 0 || recipeProcedure.length == 0) {
     return false;
   }
-
   return true;
 }
 
-async function postRecipe(recipeName, recipeOrigin, recipeImage, recipeIngredients, recipeProcedure) {
+async function postRecipe(recipeName,recipeOrigin,recipeImage,recipeIngredients,recipeProcedure) {
   try {
     const response = await fetch(urlBase, {
       method: "POST",
@@ -67,7 +70,7 @@ async function postRecipe(recipeName, recipeOrigin, recipeImage, recipeIngredien
         origin: recipeOrigin,
         image: recipeImage,
         ingredients: recipeIngredients,
-        procedure: recipeProcedure
+        procedure: recipeProcedure,
       }),
     });
 
@@ -119,7 +122,7 @@ async function printRecipes(recipes) {
   let table = document.getElementById("recipes-table");
   recipes.forEach((element) => {
     let tr = document.createElement("tr");
-    tr.innerHTML = `<td>${element.name}</td><td>${element.origin}</td><td>imagen</td><td><button onclick='getRecipeForm("${element.id}")'>Editar</button></td><td><button onclick='deleteRecipe("${element.id}")'>Eliminar</button></td>`;
+    tr.innerHTML = `<td>${element.name}</td><td>${element.origin}</td><td>imagen</td><td><button onclick="getRecipeForm('${element.id}')">Editar</button></td><td><button onclick="deleteRecipe('${element.id}')">Eliminar</button></td>`;
     table.appendChild(tr);
   });
 }
@@ -127,6 +130,82 @@ async function printRecipes(recipes) {
 async function showRecipes() {
   const recipes = await getAllRecipes();
   printRecipes(recipes);
+}
+
+// Método EDIT
+function getRecipeForm(id) {
+  console.log(id);
+  loadForm();
+  loadFieldsForm(id);
+  document.getElementById("recipe-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    editRecipe(id);
+  });
+}
+
+async function editRecipe(id) {
+  const recipeName = document.getElementById("recipe-name").value.trim();
+  const recipeOrigin = document.getElementById("recipe-origin").value.trim();
+  const recipeImage = document.getElementById("recipe-image").value.trim();
+  const recipeIngredients = document.getElementById("recipe-ingredients").value.split(",").map((ingredient) => ingredient.trim());
+  const recipeProcedure = document.getElementById("recipe-procedure").value.trim();
+  if (!validateFieldsForm(recipeName, recipeOrigin, recipeImage, recipeIngredients, recipeProcedure)) {
+    alert("Rellene correctamente todos los campos");
+    return;
+  }
+  const response = await postRecipe( recipeName, recipeOrigin, recipeImage, recipeIngredients, recipeProcedure);
+
+  if (!response.ok) {
+    alert("Error al guardar en base de datos");
+  }
+
+  resetBody();
+  const p = document.createElement("p");
+  p.textContent = "Receta modificada correctamente";
+  document.body.appendChild(p);
+}
+
+
+async function putRecipe(id,recipeName,recipeOrigin,recipeImage,recipeIngredients,recipeProcedure){
+  try {
+    const response = await fetch(urlBase + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: recipeName,
+        origin: recipeOrigin,
+        image: recipeImage,
+        ingredients: recipeIngredients,
+        procedure: recipeProcedure,
+      }),
+    });
+
+    return response;
+  } catch (error) {
+    console.log("Ha habido un error: " + error);
+  }
+}
+
+
+async function getOneRecipe(id) {
+  try {
+    const response = await fetch(urlBase + id);
+    return await response.json();
+  } catch (error) {
+    console.log("Ha habido un error: " + error);
+  }
+}
+
+async function loadFieldsForm(id) {
+  const response = await getOneRecipe(id);
+  document.getElementById("recipe-name").value = response.name;
+  document.getElementById("recipe-origin").value = response.origin;
+  document.getElementById("recipe-image").value = response.image;
+  document.getElementById("recipe-ingredients").value =
+    response.ingredients.join();
+  document.getElementById("recipe-procedure").value = response.procedure;
 }
 
 resetBody();
